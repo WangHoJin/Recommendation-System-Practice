db12b6e855fb762a5fc8baf4a10da48256c687b3
왕호진 <kkgmco0825@gmail.com>
2021-08-27 01:38:29 +0900

    matrix_factorization.py 작성

--- matrixFactorization/models/matrix_factorization.py
+++ matrixFactorization/models/matrix_factorization.py
@@ -40,16 +40,59 @@ class MF():
         self.V = np.loadtxt(output_path + '/V.dat')
     
 
-    def train(self):        
+    def train(self):
+        # Initialize user and item latent feature matrice
+        self.U = np.random.normal(scale=1./self.K, size=(self.num_users, self.K))
+        self.V = np.random.normal(scale=1./self.K, size=(self.num_movies, self.K))
+        self.T = []
+        for i, j in zip(self.R_train.nonzero()[0], self.R_train.nonzero()[1]):
+            self.T.append((i, j, self.R_train[i, j]))
+
+        # Perform stochastic gradient descent for number of iterations
+        endure_count = 5
+        count = 0
+        best_rmse = 9e7
+        training_process = []
+        for i in range(self.num_iterations):
+            np.random.shuffle(self.T)
+            self.sgd()
+            rmse = self.eval_rmse()
+            training_process.append((i, rmse))
+            print("iteration: %d ; error = %.4f" % (i + 1, rmse))
+
+            if rmse < best_rmse:
+                np.savetxt(self.output_path + '/U.dat', self.U)
+                np.savetxt(self.output_path + '/V.dat', self.V)
+                best_rmse = rmse
+                print("Best matrices are saved (err: {})".format(rmse))
+            else:
+                count = count + 1
+            if count == endure_count:
+                break
         return training_process
 
     def eval_rmse(self):
-        return rmse
+        xs, ys = self.R_valid.nonzero()
+        predicted = self.U.dot(self.V.T)
+        error = 0
+        count = 0
+        for x, y in zip(xs, ys):
+            error += pow(self.R_valid[x, y] - predicted[y, x], 2)
+            count = count + 1
+        return np.sqrt(error)/count
 
     def sgd(self):
-        pass
+        for i, j, r in self.T:
+            prediction = self.get_rating(i, j)
+            e = (r - prediction)
+
+            U_i = self.U[i, :][:]
+
+            self.U[i, :] += self.alpha * (2*e * self.V[j, :] - self.beta * self.U[i, :])
+            self.V[j, :] += self.alpha * (2*e * U_i - self.beta * self.V[j, :])
     
     def get_rating(self, i, j):
+        prediction = self.U[i, :].dot(self.V[j, :].T)
         return prediction
 
 
